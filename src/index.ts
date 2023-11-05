83558efaec6b15d4940aa8438039a4277feea8da
Bass <suddya@toptal.com>
2023-11-05 12:10:54 +0200

    mixed test passed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import ExpensesReport from "./ExpensesReport";
 import * as fs from "fs";
 
-const operationJson = fs.readFileSync('src/data/empty_operation.json', 'utf8');
+const operationJson = fs.readFileSync('src/data/mixed_operation.json', 'utf8');
 const operations: any[] = JSON.parse(operationJson);
 const report = new ExpensesReport(operations.map(o => {
     return {...o, date: new Date(o.date)}
--- src/report.test.ts
+++ src/report.test.ts
@@ -3,40 +3,26 @@ import ExpensesReport from "./ExpensesReport";
 
 describe('Excel report test', () => {
     test(`Empty records test`, () => {
-        const operationJson = fs.readFileSync('src/data/empty_operation.json', 'utf8');
-        const operations: any[] = JSON.parse(operationJson);
-        const reportJson = fs.readFileSync('src/data/empty.json', 'utf8');
-        const expectedReportRows = JSON.parse(reportJson);
-        const report = new ExpensesReport(operations.map(o => {
-            return {...o, date: new Date(o.date)}
-        }));
-        const reportRows: any[] = report.getReportRows().map(row => {
-            return {...row, date: row.date ? row.date.toISOString().split('T')[0] : null}
-        });
-        expect(expectedReportRows).toEqual(reportRows);
+        runExpensesReportTest('src/data/empty_operation.json', 'src/data/empty.json');
     });
     test(`Full records test`, () => {
-        const operationJson = fs.readFileSync('src/data/full_operation.json', 'utf8');
-        const operations: any[] = JSON.parse(operationJson);
-        const reportJson = fs.readFileSync('src/data/full.json', 'utf8');
-        const expectedReportRows = JSON.parse(reportJson);
-        const report = new ExpensesReport(operations.map(o => {
-            return {...o, date: new Date(o.date)}
-        }));
-        const reportRows: any[] = report.getReportRows().map(row => {
-            return {...row, date: row.date ? row.date.toISOString().split('T')[0] : null}
-        });
-        expect(expectedReportRows).toEqual(reportRows);
+        runExpensesReportTest('src/data/full_operation.json', 'src/data/full.json');
     });
-    // test(`Mixed records test`, () => {
-    //     const operationJson = fs.readFileSync('src/data/mixed_operation.json', 'utf8');
-    //     const operations: any[] = JSON.parse(operationJson);
-    //     const reportJson = fs.readFileSync('src/data/mixed.json', 'utf8');
-    //     const expectedReportRows = JSON.parse(reportJson);
-    //     const report = new ExpensesReport(operations.map(o => {
-    //         return {...o, date: new Date(o.date)}
-    //     }));
-    //     const reportRows = report.getReportRows();
-    //     expect(expectedReportRows).toEqual(reportRows);
-    // })
+    test(`Mixed records test`, () => {
+        runExpensesReportTest('src/data/mixed_operation.json', 'src/data/mixed.json');
+    })
 });
+
+const runExpensesReportTest = (operationsPath: string, rowsPath: string) => {
+    const operationJson = fs.readFileSync(operationsPath, 'utf8');
+    const operations: any[] = JSON.parse(operationJson);
+    const reportJson = fs.readFileSync(rowsPath, 'utf8');
+    const expectedReportRows = JSON.parse(reportJson);
+    const report = new ExpensesReport(operations.map(o => {
+        return {...o, date: new Date(o.date)}
+    }));
+    const reportRows: any[] = report.getReportRows().map(row => {
+        return {...row, date: row.date ? row.date.toISOString().split('T')[0] : null}
+    });
+    expect(expectedReportRows).toEqual(reportRows);
+};
